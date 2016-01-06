bdf24c7c3f8c8b9253dfa169ed3304b60490038f
Sam Caldwell <samcaldwell@utexas.edu>
2016-01-06 12:22:57 -0500

    add MF publications

--- publications.rkt
+++ publications.rkt
@@ -11,34 +11,34 @@
 @(define POPL "Principles of Programming Languages (POPL)")
 @(define ESOP "European Symposium on Programming (ESOP)")
 @(define JFP "Journal of Functional Programming (JFP)")
+@(define ECOOP "European Conference on Object-Oriented Programming (ECOOP)")
+@(define SNAPL "Summit on Advances in Programming Langugages (SNAPL)")
+@(define CC "International Conference on Compiler Construction (CC)")
+@(define SIGCSE "SIGCSE")
+@(define TOPLAS "Transactions on Programming Languages and Systems (TOPLAS)")
+@(define IFL "Implementation and Application of Functional Languages(IFL)")
+@(define PADL "Practical Aspects of Declarative Languages (PADL)")
+@(define TFP "Trends in Functional Programming (TFP)")
+@(define DLS "Dynamic Languages Symposium (DLS)")
+@(define TLDI "Types in Language Design and Implementation (TLDI)")
+@(define Scheme "Scheme and Functional Programming Workshop")
+@(define PPDP "Principles and Practice of Declarative Programming (PPDP)")
+@(define ACL2 "ACL2 Workshop")
+@(define FDPE "Functional and Declarative Programming in Education (FDPE)")
+@(define GPCE "Generative Programming: Concepts & Experience (GPCE)")
 
-@(define publications
+@(define old-site-pubs
    (list
-    (publication "Typing the numeric tower"
-                 "Vincent St-Amour, Sam Tobin-Hochstadt, Matthew Flatt and Matthias Felleisen"
-                 "Practical Aspects of Declarative Languages (PADL)"
-                 2012
-                 "http://www.ccs.neu.edu/home/stamourv/papers/numeric-tower.pdf")
     (publication "Seeing the futures: profiling shared-memory parallel Racket"
                  "James Swaine, Burke Fetscher, Vincent St-Amour, Robby Findler and Matthew Flatt"
                  "Functional High-Performance Computing (FHPC)"
                  2012
                  "http://www.ccs.neu.edu/home/stamourv/papers/seeing-the-futures.pdf")
-    (publication "Optimization coaching"
-                 "Vincent St-Amour, Sam Tobin-Hochstadt and Matthias Felleisen"
-                 OOPSLA
-                 2012
-                 "http://www.ccs.neu.edu/home/stamourv/papers/optimization-coaching.pdf")
     (publication "Practical Programming with Substructural Types"
                  "Jesse A. Tov"
                  "PhD Dissertation, Northeastern University"
                  2012
                  "http://users.eecs.northwestern.edu/~jesse/pubs/dissertation/")
-    (publication "Languages as libraries"
-                 "Sam Tobin-Hochstadt, Vincent St-Amour, Ryan Culpepper, Matthew Flatt and Matthias Felleisen"
-                 PLDI
-                 2011
-                 "http://www.ccs.neu.edu/home/stamourv/papers/lang-lib.pdf")
     (publication "A Family of Abstract Interpretations for Static Analysis of Concurrent Higher-Order Programs"
                  "Matthew Might and David Van Horn"
                  "The 18th International Static Analysis Symposium"
@@ -86,7 +86,7 @@
                  "http://www.ccs.neu.edu/home/dvanhorn/pubs/might-smaragdakis-vanhorn-pldi10.pdf")
     (publication "Evaluating Call By Need on the Control Stack"
                  "Stephen Chang, David Van Horn and Matthias Felleisen"
-                 "Trends in Functional Programming"
+                 TFP
                  2010
                  "http://www.ccs.neu.edu/home/stchang/pubs/Chang-VanHorn-Felleisen-TFP2010.pdf")
     (publication "CFA2: a Context-Free Approach to Control-Flow Analysis"
@@ -171,34 +171,14 @@
                  "http://www.ccs.neu.edu/racket/pubs/dissertation-cobbe.pdf")
     (publication "Metaprogramming with Traits"
                  "John Reppy and Aaron Turon"
-                 "European Conference on Object-Oriented Programming (ECOOP)"
+                 ECOOP
                  2007
                  "http://www.ccs.neu.edu/home/turon/ecoop07-meta-traits.pdf")
-    (publication "ACL2 for Freshmen: First Experiences"
-                 "Carl Eastlund, Dale Vaillancourt and Matthias Felleisen"
-                 "ACL2 Workshop"
-                 2007
-                 "http://www.ccs.neu.edu/racket/pubs/acl207-evf.pdf")
-    (publication "Debugging Macros"
-                 "Ryan Culpepper and Matthias Felleisen"
-                 "GPCE"
-                 2007
-                 "http://www.ccs.neu.edu/racket/pubs/gpce07-cf.pdf")
     (publication "Advanced Macrology and the Implementation of Typed Scheme"
                  "Ryan Culpepper, Sam Tobin-Hochstadt and Matthew Flatt"
                  "Scheme Workshop"
                  2007
                  "http://www.ccs.neu.edu/racket/pubs/scheme2007-ctf.pdf")
-    (publication "Adding Delimited and Composable Control to a Production Programming Environment"
-                 "Matthew Flatt, Gang Yu, Robert Bruce Findler and Matthias Felleisen"
-                 ICFP
-                 2007
-                 "http://www.ccs.neu.edu/scheme/pubs/icfp07-fyff.pdf")
-    (publication "Implementation and Use of the PLT Scheme Web Server"
-                 "Shriram Krishnamurthi, Peter Walton Hopkins, Jay McCarthy, Paul Graunke, Greg Pettyjohn and Matthias Felleisen"
-                 "Journal of Higher-Order and Symbolic Computing"
-                 2007
-                 "http://www.ccs.neu.edu/scheme/pubs/hosc07-sk-mf.pdf")
     (publication "Relating Complexity and Precision in Control Flow Analysis"
                  "David Van Horn and Harry G. Mairson"
                  ICFP
@@ -216,9 +196,221 @@
                  "http://www.ccs.neu.edu/home/dherman/research/papers/icfp07-great-escape.pdf")
     (publication "Space-Efficient Gradual Typing"
                  "David Herman, Aaron Tomb and Cormac Flanagan"
-                 "Trends in Functional Programming"
+                 TFP
                  2007
                  "http://www.ccs.neu.edu/home/dherman/research/papers/tfp07-gradual-typing.pdf")))
+
+@(define mf-pubs
+   (list
+     (publication "Is sound gradual typing dead?"
+                  "Asumu Takikawa, Daniel Feltey, Ben Greenman, Max S. New, Jan Vitek, and Matthias Felleisen"
+                  POPL
+                  2016
+                  "http://www.ccs.neu.edu/racket/pubs/popl16-tfgnvf.pdf")
+     (publication "Toward practical gradual typing"
+                   "Asumu Takikawa, Daniel Feltey, Earl Dean, Matthew Flatt, Robert Bruce Findler, Sam Tobin-Hochstadt, and Matthias Felleisen"
+                   ECOOP
+                   2015
+                   "http://www.ccs.neu.edu/racket/pubs/ecoop2015-takikawa-et-al.pdf")
+     (publication "The Racket Manifesto"
+                  "Matthias Felleisen, Robert Bruce Findler, Matthew Flatt, Shriram Krishnamurthi, Eli Barzilay, Jay McCarthy, Sam Tobin-Hochstadt"
+                  SNAPL
+                  2015
+                  "http://www.ccs.neu.edu/racket/pubs/manifesto.pdf")
+     (publication "Feature-specific Profiling"
+                  "Vincent St-Amour, Leif Andersen, Matthias Felleisen"
+                  CC
+                  2015
+                  "http://www.ccs.neu.edu/racket/pubs/cc15-saf.pdf")
+     (publication "Transferring Skills at Solving Word Problems from Computing to Algebra Through Bootstrap"
+                  "Emmanuel Schanzer, Kathi Fisler, Shriram Krishnamurthi, Matthias Felleisen"
+                  SIGCSE
+                  2015
+                  "http://www.ccs.neu.edu/racket/pubs/sigcse-sfkf.pdf")
+     (publication "The Network as a Language Construct"
+                  "Tony Garnock-Jones, Sam Tobin-Hochstadt, and Matthias Felleisen"
+                  ESOP
+                  2014
+                  "http://www.ccs.neu.edu/racket/pubs/esop14-gjthf.pdf")
+     (publication "Profiling for Laziness"
+                  "Stephen Chang, Matthias Felleisen"
+                  POPL
+                  2014
+                  "http://www.ccs.neu.edu/racket/pubs/popl14-cf.pdf")
+     (publication "Contracts for First-Class Classes"
+                  "T. Stephen Strickland, Christos Dimoulas, Asumu Takikawa, and Matthias Felleisen"
+                  TOPLAS
+                  2013
+                  "http://www.ccs.neu.edu/racket/pubs/toplas13-sdtf.pdf")
+     (publication "Option Contracts"
+                  "Christos Dimoulas, Robert Bruce Findler, Matthias Felleisen"
+                  OOPSLA
+                  2013
+                  "http://www.ccs.neu.edu/racket/pubs/oopsla13-dff.pdf")
+     (publication "Gradual Typing for First-Class Classes"
+                  "Asumu Takikawa, T. Stephen Strickland, Christos Dimoulas, Sam Tobin-Hochstadt, Matthias Felleisen"
+                  OOPSLA
+                  2012
+                  "http://www.ccs.neu.edu/racket/pubs/oopsla12-tsdthf.pdf")
+     (publication "Optimization Coaching"
+                  "Vincent St-Amour, Sam Tobin-Hochstadt, Matthias Felleisen"
+                  OOPSLA
+                  2012
+                  "http://www.ccs.neu.edu/racket/pubs/oopsla12-stf.pdf")
+     (publication "The Call-by-need Lambda Calculus, Revisited"
+                  "Stephen Chang and Matthias Felleisen"
+                  ESOP
+                  2012
+                  "http://www.ccs.neu.edu/racket/pubs/esop12-cf.pdf")
+     (publication "Complete Monitors for Behavioral Contracts"
+                  "Christos Dimoulas, Sam Tobin-Hochstadt, and Matthias Felleisen"
+                  ESOP
+                  2012
+                  "http://www.ccs.neu.edu/racket/pubs/esop12-dthf.pdf")
+     (publication "From Stack Traces to Lazy Rewriting Sequences"
+                  "Stephen Chang, Eli Barzilay, John Clements, Matthias Felleisen"
+                  IFL
+                  2011
+                  "http://www.ccs.neu.edu/racket/pubs/ifl11-cbcf.pdf")
+     (publication "On Contract Satisfaction in a Higher-Order World"
+                  "Christos Dimoulas, Matthias Felleisen"
+                  TOPLAS
+                  2011
+                  "http://www.ccs.neu.edu/racket/pubs/df-toplas11.pdf")
+     (publication "Run Your Research"
+                  "Casey Klein, John Clements, Christos Dimoulas, Carl Eastlund, Matthias Felleisen, Matthew Flatt, Jay McCarthy, Jon Rafkind, Sam Tobin-Hochstadt, Robert Bruce Findler"
+                  POPL
+                  2012
+                  "http://eecs.northwestern.edu/~robby/lightweight-metatheory/popl2012-kcdeffmrtf.pdf")
+     (publication "Typing the Numeric Tower"
+                  "Vincent St-Amour, Sam Tobin-Hochstadt, Matthew Flatt, and Matthias Felleisen"
+                  PADL
+                  2012
+                  "http://www.ccs.neu.edu/racket/pubs/padl12-stff.pdf")
+     (publication "Languages as Libraries"
+                  "Sam Tobin-Hochstadt, Vincent St-Amour, Ryan Culpepper, Matthew Flatt, Matthias Felleisen"
+                  PLDI
+                  2011
+                  "http://www.ccs.neu.edu/racket/pubs/pldi11-thacff.pdf")
+     (publication "Correct Blame for Contracts: No More Scapegoating"
+                  "Christos Dimoulas, Robert Bruce Findler, Cormac Flanagan, Matthias Felleisen"
+                  POPL
+                  2011
+                  "http://www.ccs.neu.edu/racket/pubs/popl11-dfff.pdf")
+     (publication "Hygienic Macros for ACL2"
+                  "Carl Eastlund, Matthias Felleisen"
+                  TFP
+                  2010
+                  "http://www.ccs.neu.edu/racket/pubs/tfp10-ef.pdf")
+     (publication "Contracts for First-Class Classes"
+                  "T. Stephen Strickland, Matthias Felleisen"
+                  DLS
+                  2010
+                  "http://www.ccs.neu.edu/racket/pubs/dls10-sf.pdf")
+     (publication "Fortifying Macros"
+                  "Ryan Culpepper, Matthias Felleisen"
+                  ICFP
+                  2010
+                  "http://www.ccs.neu.edu/racket/pubs/icfp10-cf.pdf")
+     (publication "Logical Types for Untyped Languages"
+                  "Sam Tobin-Hochstadt, Matthias Felleisen"
+                  ICFP
+                  2010
+                  "http://www.ccs.neu.edu/racket/pubs/icfp10-thf.pdf")
+     (publication "TeachScheme!---A Checkpoint (Abstract)"
+                  "Matthias Felleisen"
+                  ICFP
+                  2010
+                  "http://www.ccs.neu.edu/racket/pubs/icfp10-f.pdf")
+     (publication "Adding Types to Untyped Languages (Abstract)"
+                  "Matthias Felleisen"
+                  TLDI
+                  2010
+                  "http://www.ccs.neu.edu/racket/pubs/tldi10-f.pdf")
+     (publication "Nested and Dynamic Contract Boundaries"
+                  "T. Stephen Strickland, Matthias Felleisen"
+                  IFL
+                  2009
+                  "http://www.ccs.neu.edu/racket/pubs/ifl09-sf.pdf")
+     (publication "Contracts for First-Class Modules"
+                  "T. Stephen Strickland, Matthias Felleisen"
+                  DLS
+                  2009
+                  "http://www.ccs.neu.edu/racket/pubs/dls09-sf.pdf")
+     (publication "Sequence Traces for Object-Oriented Executions"
+                  "Carl Eastlund, Matthias Felleisen"
+                  Scheme
+                  2009
+                  "http://www.ccs.neu.edu/racket/pubs/scheme2009-ef.pdf")
+     (publication "Making Induction Manifest in Modular ACL2"
+                  "Carl Eastlund, Matthias Felleisen"
+                  PPDP
+                  2009
+                  "http://www.ccs.neu.edu/racket/pubs/ppdp09-ef.pdf")
+     (publication "Future Contracts"
+                  "Christos Dimoulas, Riccardo Pucella, Matthias Felleisen"
+                  PPDP
+                  2009
+                  "http://www.ccs.neu.edu/racket/pubs/ppdp09-dpf.pdf")
+     (publication "Ryan Culpepper, Matthias Felleisen"
+                  "Debugging Hygienic Macros"
+                  "Science of Computer Programming"
+                  2009
+                  "http://www.ccs.neu.edu/racket/pubs/cf-sp09.pdf")
+     (publication "A Functional I/O System (or Fun for Freshman Kids)"
+                  "Matthias Felleisen, Robert Bruce Findler, Matthew Flatt, Shriram Krishnamurthi"
+                  ICFP
+                  2009
+                  "http://www.ccs.neu.edu/racket/pubs/icfp09-fffk.pdf")
+     (publication "Automatic Verification for Interactive Graphical Programs"
+                  "Carl Eastlund and Matthias Felleisen"
+                  ACL2
+                  2009
+                  "http://www.ccs.neu.edu/racket/pubs/acl209-ef.pdf")
+     (publication "Practical Variable-Arity Polymorphism"
+                  "T. Stephen Strickland, Sam Tobin-Hochstadt, and Matthias Felleisen"
+                  ESOP
+                  2009
+                  "http://www.ccs.neu.edu/racket/pubs/esop09-sthf.pdf")
+     (publication "Toward a Practical Module System for ACL2"
+                  "Carl Eastlund and Matthias Felleisen"
+                  PADL
+                  2009
+                  "http://www.ccs.neu.edu/racket/pubs/padl09-ef.pdf")
+     (publication "Functional Programming and Theorem Proving for Undergraduates: A Progress Report"
+                  "Rex Page, Carl Eastlund, and Matthias Felleisen"
+                  FDPE
+                  2008
+                  "http://www.ccs.neu.edu/racket/pubs/fdpe08-pef.pdf")
+     (publication "The Design and Implementation of Typed Scheme"
+                  "Sam Tobin-Hochstadt, Matthias Felleisen"
+                  POPL
+                  2008
+                  "http://www.ccs.neu.edu/racket/pubs/popl08-thf.pdf")
+     (publication "ACL2 for Freshmen: First Experiences"
+                  "Carl Eastlund, Dale Vaillancourt, Matthias Felleisen"
+                  ACL2
+                  2007
+                  "http://www.ccs.neu.edu/racket/pubs/acl207-evf.pdf")
+     (publication "Debugging Macros"
+                  "Ryan Culpepper, Matthias Felleisen"
+                  GPCE
+                  2007
+                  "http://www.ccs.neu.edu/racket/pubs/gpce07-cf.pdf")
+     (publication "Adding Delimited and Composable Control to a Production Programming Environment"
+                  "Matthew Flatt, Gang Yu, Robert Bruce Findler, Matthias Felleisen"
+                  ICFP
+                  2007
+                  "http://www.ccs.neu.edu/racket/pubs/icfp07-fyff.pdf")
+     (publication "Implementation and Use of the PLT Scheme Web Server"
+                  "Shriram Krishnamurthi, Peter Walton Hopkins, Jay McCarthy, Paul T. Graunke, Greg Pettyjohn, Matthias Felleisen"
+                  "Journal of Higher-Order and Symbolic Computing"
+                  2007
+                  "http://www.ccs.neu.edu/racket/pubs/hosc07-sk-mf.pdf")))
+
+@(define publications
+   (flatten (list mf-pubs
+                  old-site-pubs)))
 
 @(define (publication->html pub)
    (match-define (publication title authors venue year link) pub)
