32834eaaea9b1c90e6f550f45f0119e575db510e
Daniel Patterson <dbp@dbpmail.net>
2017-12-12 16:58:27 -0500

    add seminar for Molham Aref

--- seminars.rkt
+++ seminars.rkt
@@ -78,6 +78,17 @@
     ;;  @list{@p{BIO}})
 
     (seminar
+     "aref-declarative"
+     "Solver-Aided Declarative Programming"
+     "Molham Aref"
+     "#"
+     "Relational AI"
+     (datetime 2017 12 14 10 00)
+     "WVH 366"
+     @list{@p{I will summarize our work on a declarative programming language that offers native language support for model (or instance) finding. This capability can be used to express predictive (e.g. machine learning) and prescriptive (e.g. combinatorial optimization) analytics. The presentation gives an overview of the platform and the language. In particular, it focuses on the important role of integrity constraints,which are used not only for maintaining data integrity, but also, for the formal specification of complex optimization problems and probabilistic programming.}}
+     @list{@p{Mr. Molham Aref is the Chief Executive Officer of Relational AI. Mr. Aref has more than 25 years of experience in developing and implementing enterprise-grade analytic, predictive, optimization and simulation solutions for the demand chain, supply chain, and revenue management across various industries. Relational AI combines the latest advances in Artificial Intelligence with a understanding of business processes to develop solutions that shape better decisions, improve agility, and reduce risk. Prior to Relational AI, he was co-founder and CEO of LogicBlox where he led the company from inception through a successful sale to Infor. Previously, he was CEO of Optimi (acquired by Ericsson),a leader in wireless network simulation and optimization, and co-founder of Brickstream (renamed Nomi and then acquired by FLIR), a leading provider of computer-vision-based behavior intelligence solutions.}})
+
+    (seminar
      "martens-compositional"
      "Compositional Creativity"
      "Chris Martens"
