06982c7a85bd2ecb586bcbda9f06fd0c7468d89d
Aviral Goel <goel.aviral@gmail.com>
2018-09-25 12:09:34 -0400

    Seminar | Add Michael Greenberg's 10th October seminar details

--- seminars.rkt
+++ seminars.rkt
@@ -78,6 +78,17 @@
     ;;  @list{@p{BIO}})
 
     (seminar
+     "greenberg-posix-shell"
+     "Rehabilitating the POSIX shell"
+     "Michael Greenberg"
+     "http://www.cs.pomona.edu/~michael/"
+     "Pomona College"
+     (datetime 2018 10 10 10 0)
+     "WVH 366"
+     @list{@p{ We build intricate systems with complex algorithms and invariants, aiming for guarantees of correctness and performance... and then we maintain and deploy these systems with shell scripts! What *are* shell scripts? If the POSIX shell is a programming language, what are its syntax and semantics? Can we apply PL tools to reason about the shell? Why haven't prior PL attempts at understanding the shell redeemed it?}}
+     @list{@p{Michael Greenberg is an assistant professor at Pomona College. He received his BA in Computer Science and Egyptology from Brown University (2007) and his PhD in Computer Science from the University of Pennsylvania (2013). His work has ranged from functional-reactive JavaScript (with Shriram Krishnamurthi at Brown) to runtime verification of higher-order programs using contracts (with Benjamin Pierce at Penn) to software-defined networking (with Dave Walker at Princeton) to present activities focused on Kleene algebra with tests and the POSIX shell. He is always looking for new climbing partners.}})
+
+    (seminar
      "cifuentes-parfait-vulnerability-detection"
      "Oracle Parfait: The Flavour of Real-World Vulnerability Detection"
      "Cristina Cifuentes"
