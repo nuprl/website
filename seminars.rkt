b6c4321b4b096edaf5c0fdca2159a16cf587c141
Aviral Goel <goel.aviral@gmail.com>
2019-01-22 14:26:12 -0500

    Seminar | Add Lucas Waye's 6th February seminar details

--- seminars.rkt
+++ seminars.rkt
@@ -81,17 +81,6 @@
     ;;  @list{@p{ABSTRACT}}
     ;;  @list{@p{BIO}})
 
-    ;; (seminar
-    ;;  "waye-whip-contract-service"
-    ;;  "Whip: Higher-order Contracts for Modern Services"
-    ;;  "Lucas Waye"
-    ;;  "http://lucaswaye.com/"
-    ;;  "Facebook"
-    ;;  (datetime 2018 12 12 10 0)
-    ;;  "WVH 366"
-    ;;  @list{@p{Modern service-oriented applications forgo semantically rich protocols and middleware when composing services. Instead, they embrace the loosely-coupled development and deployment of services that communicate via simple network protocols. Even though these applications do expose interfaces that are higher-order in spirit, the simplicity of the network protocols forces them to rely on brittle low-level encodings. To bridge the apparent semantic gap, programmers introduce ad-hoc and error-prone defensive code. Inspired by Design by Contract, we choose a different route to bridge this gap. We introduce Whip, a contract system for modern services. Whip (i) provides programmers with a higher-order contract language tailored to the needs of modern services; and (ii) monitors services at run time to detect services that do not live up to their advertised interfaces. Contract monitoring is local to a service. Services are treated as black boxes, allowing heterogeneous implementation languages without modification to services' code. Thus, Whip does not disturb the loosely coupled nature of modern services.}}
-    ;;  @list{@p{I'm currently a Software Engineer at Facebook thinking about privacy in the data warehouse. Previously I helped build TiVo's Targeted Audience Delivery platform. And before that I worked on the backend for Timeful (acquired by Google) and on Hulu's analytics platform. In graduate school I worked on Whip, a contract monitor for microservices. I received a Ph.D. and Master's degree from Harvard under the guidance of Stephen Chong and a Bachelor's degree from Cornell.}})
-
     (seminar
      "hur"
      "TBA"
@@ -113,6 +102,17 @@
      "WVH 366"
      @list{@p{}}
      @list{@p{}})
+
+    (seminar
+     "waye-whip-contract-service"
+     "Whip: Higher-order Contracts for Modern Services"
+     "Lucas Waye"
+     "http://lucaswaye.com/"
+     "Facebook"
+     (datetime 2019 2 6 10 0)
+     "WVH 366"
+     @list{@p{Modern service-oriented applications forgo semantically rich protocols and middleware when composing services. Instead, they embrace the loosely-coupled development and deployment of services that communicate via simple network protocols. Even though these applications do expose interfaces that are higher-order in spirit, the simplicity of the network protocols forces them to rely on brittle low-level encodings. To bridge the apparent semantic gap, programmers introduce ad-hoc and error-prone defensive code. Inspired by Design by Contract, we choose a different route to bridge this gap. We introduce Whip, a contract system for modern services. Whip (i) provides programmers with a higher-order contract language tailored to the needs of modern services; and (ii) monitors services at run time to detect services that do not live up to their advertised interfaces. Contract monitoring is local to a service. Services are treated as black boxes, allowing heterogeneous implementation languages without modification to services' code. Thus, Whip does not disturb the loosely coupled nature of modern services.}}
+     @list{@p{I'm currently a Software Engineer at Facebook thinking about privacy in the data warehouse. Previously I helped build TiVo's Targeted Audience Delivery platform. And before that I worked on the backend for Timeful (acquired by Google) and on Hulu's analytics platform. In graduate school I worked on Whip, a contract monitor for microservices. I received a Ph.D. and Master's degree from Harvard under the guidance of Stephen Chong and a Bachelor's degree from Cornell.}})
 
     (seminar
      "fare-better-stories-reframing-from-programs-to-programming"
