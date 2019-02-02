7b58122d9a7dd3bf6bc320aa8ec7de949b05e92c
Aviral Goel <goel.aviral@gmail.com>
2019-02-02 11:14:51 -0500

    Seminar | Update Ben Titzer's 13th February seminar location

--- seminars.rkt
+++ seminars.rkt
@@ -121,7 +121,7 @@
      "http://research.google.com/pubs/BenTitzer.html"
      "Google"
      (datetime 2019 2 13 10 0)
-     "WVH 366"
+     "Ryder 155"
      @list{@p{Until now, CPU designers and computer scientists have assumed Vegas rules at the hardware level: what happens in speculation stays in speculation. Yet in the wake of the Spectre and Meltdown attacks, it has become clear a new, massive class of security vulnerabilities awaits us at the microarchitectural level because this assumption is simply false. As language designers and implementors familiar with building towers of abstractions, we have assumed that virtualization through emulation made the worlds below the Turing machine undetectable, hidden behind a mathematically perfect mirror. This talk will explore how we have now learned to see through that mirror, into the very bizarre and alien world of microarchitectures, illuminating a tiny world of astounding complexity that holds profound implications for language security.}}
      @list{@p{Ben L. Titzer leads Google's WebAssembly team in Munich. Before starting the WebAssembly project with Luke Wagner from Mozilla, he led the team that built the TurboFan optimizing compiler which now powers JavaScript and WebAssembly in V8. He graduated with a BS from Purdue University in 2002 and MS and PhD from UCLA in 2004 and 2007. His interests include safe programming languages for systems programming, compilers, virtual machines, nature and playing guitar.}})
 
