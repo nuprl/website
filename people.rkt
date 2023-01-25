f16fa84e6709738ff9ffde3bb06949deeae81aee
Andrew Wagner <ahwagner@outlook.com>
2023-01-25 08:26:48 -0500

    remove dupl hemann, move oli to alumni

--- people.rkt
+++ people.rkt
@@ -264,15 +264,6 @@
             I usually work on types, currently focusing on gradual type systems.
           }
 
-          @person[#:name "Olivier Flückiger"
-                  #:title "Advisor: Jan Vitek"
-                  #:e-mail "o@o1o.ch"
-                  #:website "http://www.o1o.ch/about"
-                  #:history @list["Joined Northeastern, 2015"]
-                  #:img "olivier_fluckiger.jpg"]{
-            My passion lies in language implementation @|mdash| creating the tools and techniques required to get from powerful abstractions to efficient execution.
-          }
-
           @person[#:name "Ming-Ho Yee"
                   #:title "Advisor: Arjun Guha"
                   #:e-mail "mhyee@ccs.neu.edu"
@@ -702,6 +693,9 @@
               @(alumnus "Leif Andersen" #:year 2022
                       #:personal-site "https://leifandersen.net/index.html"
                       #:extra "(UMass Boston)")
+              @(alumnus "Olivier Flückiger" #:year 2022
+                      #:personal-site "http://www.o1o.ch/about"
+                      #:dissertation "https://thesis.r-vm.net/main.pdf")
               @(alumnus "Daniel Patterson" #:year 2022
                         #:dissertation "https://dbp.io/pubs/2022/dbp-dissertation.pdf"
                         #:extra "(Northeastern University)"
@@ -801,8 +795,6 @@
                 @(alumnus "Heather Miller" #:year 2018
                           #:personal-site "http://heather.miller.am/"
                           #:extra "(Carnegie Mellon University)")
-                @(alumnus "Jason Hemann" #:year 2022
-                          #:extra "(Seton Hall)")
               ]}}
         @footer{}
 }}}}
